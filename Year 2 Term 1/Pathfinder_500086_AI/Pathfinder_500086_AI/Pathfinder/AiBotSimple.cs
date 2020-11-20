32472af6090609052a124d73de6d5bd7a518661e
DanBates <djbates@mail.com>
2020-11-20 15:33:08 +0000

    Added AI coursework
    In this courswork I will only be working on the AiBotAStar.cs and the AiBotLRTAStar.cs classes

---
+++ Year 2 Term 1/Pathfinder_500086_AI/Pathfinder_500086_AI/Pathfinder/AiBotSimple.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathfinder
+{
+    class AiBotSimple : AiBotBase
+    {
+        Random rnd;
+        public AiBotSimple(int x, int y) : base(x, y)
+        {
+            rnd = new Random();
+        }
+
+        protected override void ChooseNextGridLocation(Level level, Player plr)
+        {
+            bool ok = false;
+            Coord2 pos;// = new Coord2();
+            while (!ok)
+            {
+                pos = GridPosition;
+                int x = rnd.Next(0, 4);
+                switch (x)
+                {
+                    case (0):
+                        pos.X += 1;
+                        break;
+                    case (1):
+                        pos.X -= 1;
+                        break;
+                    case (2):
+                        pos.Y += 1;
+                        break;
+                    case (3):
+                        pos.Y -= 1;
+                        break;
+                }
+                ok = SetNextGridPosition(pos, level);
+            }
+        }
+    }
+}
