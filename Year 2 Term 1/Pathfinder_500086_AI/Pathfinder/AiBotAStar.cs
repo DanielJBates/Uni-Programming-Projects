578489fa051d3689ee65fa62a4128cb4655bd27d
DanBates <djbates@mail.com>
2020-11-28 13:26:19 +0000

    Graph.cs & Folder tidy
    Added the Graph.cs which we were given and I clean up the file location as there was a duplicate folder

---
+++ Year 2 Term 1/Pathfinder_500086_AI/Pathfinder/AiBotAStar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathfinder
+{
+    class AiBotAStar: AiBotBase
+    {
+        public AiBotAStar(int x, int y): base(x,y)
+        {
+        }
+        protected override void ChooseNextGridLocation(Level level, Player plr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Year 2 Term 1/Pathfinder_500086_AI/Pathfinder/AiBotLRTAStar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathfinder
+{
+    class AiBotLRTAStar: AiBotBase
+    {
+        public AiBotLRTAStar(int x, int y): base(x,y)
+        {
+        }
+
+        protected override void ChooseNextGridLocation(Level level, Player plr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
