da7002bb0adf6f1870e7360657e1395299782231
Daniel Bates <68301407+DanielJBates@users.noreply.github.com>
2020-12-09 16:36:24 +0000

    Done more work on the help sessions
    Trying to translate the stuff from the help session into code the is more understandable to me

--- Year 2 Term 1/Pathfinder_500086_AI/Pathfinder/AiBotAStar.cs
+++ Year 2 Term 1/Pathfinder_500086_AI/Pathfinder/AiBotAStar.cs
@@ -12,7 +12,8 @@ namespace Pathfinder
         Coord2 mTargetPos;
         int mGridSize;
         double[,] mGraph;
-        List<Node> nodes;
+        List<Node> mNodes;
+        Dictionary<string, Node> mPathTracking;
 
         public AiBotAStar(int x, int y, double[,] pGraphMatrix, int pGridSize) : base(x, y)
         {
@@ -20,11 +21,14 @@ namespace Pathfinder
             mTargetPos = new Coord2(x, y);
             mGraph = pGraphMatrix;
             mGridSize = pGridSize;
-            nodes = new List<Node>();
+            mNodes = new List<Node>();
+            mPathTracking = new Dictionary<string, Node>();
         }
         protected override void ChooseNextGridLocation(Level level, Player plr)
         {
-
+            mTargetPos = plr.GridPosition;
+            double h_n = Calculate_h_n(mStartPos, mTargetPos);
+            this.Build();
         }
         public void Build()
         {
@@ -34,14 +38,15 @@ namespace Pathfinder
             startNode.g_n = 0;
             startNode.position = mStartPos;
             startNode.index = PositionToVertex(startNode.position);
-            startNode.h_n = CalculateH_n(startNode.position, mTargetPos);
+            startNode.h_n = Calculate_h_n(startNode.position, mTargetPos);
             startNode.f_n = startNode.g_n + startNode.h_n;
 
-            nodes.Add(startNode);
+            mNodes.Add(startNode);
+            mPathTracking.Add(startNode.index + "-" + startNode.level, startNode);
 
             Node currentNode = startNode;
 
-            while (nodes.Count > 0)
+            while (mNodes.Count > 0)
             {
 
                 for (int i = 0; i < mGraph.GetLength(1); i++)
@@ -53,13 +58,15 @@ namespace Pathfinder
                         newNode.parent = currentNode.index;
                         newNode.g_n = currentNode.g_n + mGraph[currentNode.index, i];
                         newNode.level = currentNode.level + 1;
+                        newNode.index = i;
                         newNode.position = VertexToPosition(i);
-                        newNode.h_n = CalculateH_n(newNode.position, mTargetPos);
+                        newNode.h_n = Calculate_h_n(newNode.position, mTargetPos);
                         newNode.f_n = newNode.g_n + newNode.h_n;
 
-                        nodes.Add(newNode);
+                        mNodes.Add(newNode);
+                        mPathTracking.Add(newNode.index + "-" + newNode.level, newNode);
                     }
-                    nodes.Remove(currentNode);
+                    mNodes.Remove(currentNode);
                     currentNode = getMinVertex();
                     if (currentNode.position == mTargetPos)
                     {
@@ -72,7 +79,7 @@ namespace Pathfinder
             {
                 double min = int.MaxValue;
                 Node v = null;
-                foreach (Node n in nodes)
+                foreach (Node n in mNodes)
                 {
                     if (n.f_n < min)
                     {
@@ -94,7 +101,7 @@ namespace Pathfinder
 
                 return Pos;
             }
-            private double CalculateH_n(Coord2 pStart, Coord2 pTarget)
+            private double Calculate_h_n(Coord2 pStart, Coord2 pTarget)
             {
                 return Math.Sqrt(Convert.ToDouble(((pTarget.X - pStart.X) * (pTarget.X - pStart.X)) + ((pTarget.Y - pStart.Y) * (pTarget.Y - pStart.Y))));
             }
